5d30b4db9109b69c8d7cda8b4b586751803d3685
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2020-05-08 19:30:49 +0200

    First version
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="qdjarv",
+      version="0.1",
+      description="Quick and dirty jsonapi response validator",
+      url="https://github.com/Wesmania/qdjarv",
+      author="Igor Kotrasinski",
+      license='MIT',
+      packages=['qdjarv'])
